651355f1c7e84225ba3df79b3929eeded2496025
zosarillana <zhou991818@gmail.com>
2024-08-28 09:29:39 +0800

    added new logos and loading screen

---
+++ src/app/components/admin/views/master-data/area-add/area-add.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-area-add',
+  templateUrl: './area-add.component.html',
+  styleUrl: './area-add.component.css'
+})
+export class AreaAddComponent {
+
+}
---
+++ src/app/components/admin/views/master-data/pod-add/pod-add.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pod-add',
+  templateUrl: './pod-add.component.html',
+  styleUrl: './pod-add.component.css'
+})
+export class PodAddComponent {
+
+}
